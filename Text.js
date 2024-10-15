d75f0aab8199f139284a357585505828e55aa618
B-4a <bruk33@proton.me>
2024-10-15 18:20:29 -0400

    Add files via upload

---
+++ Text.js
@@ -0,0 +1,27 @@
+    const URL = "http://github.com/b-4a/b-4a1.git"
+
+   fetch("URL")
+
+      .then(function(response) {
+         console.log(response);
+        return response.json();
+   })
+
+      .then(function(data){
+  
+         console.log(data);
+const 
+img1 = document.queryselector('./submit');
+button =
+document.queryselector('./{length});
+
+
+button.eventListner('click', function length(){
+if(i<1, i<value.{length}); 
+(i={'length'});
+
+
+img1==JSON.stringify(['value.{i}'])
+
+console.log('$img1.value.{length}$');
+};
